a6bc2e2d7fe707264df766a98ae6c0c8ba83030b
JoshuaMK <60854312+JoshuaMKW@users.noreply.github.com>
2025-04-08 14:48:23 -0500

    Update offsets

--- include/JSystem/JParticle/JPABaseEmitter.hxx
+++ include/JSystem/JParticle/JPABaseEmitter.hxx
@@ -28,14 +28,12 @@ public:
     void setGlobalRTMatrix(Mtx);
     void setGlobalSRTMatrix(Mtx);
 
-    u32 _0C[0xE8 / 4];
-    JSUPtrList mParticleList;  // _F4
+    u32 _10[0xE4 / 4];
+    JSUPtrList mParticleList;       // _F4
     JSUPtrList mChildParticleList;  // _100
-    u32 _10C[0x50 / 4];
-    TVec3f mSize1;
-    u32 _160;
-    TVec3f mSize2;
-    u32 _170;
-    TVec3f mSize3;
+    u32 _10C[0x48 / 4];
+    TVec3f mSize1;  // _154
+    u32 _160[0x14 / 4];
+    TVec3f mSize3;  // _174
     u32 _180[0xB0 / 4];
 };
